2a918f0dbed1a5f16e49983f53bff8aab23d8d83
Aman Rajak <111343496+amanworld8040@users.noreply.github.com>
2024-08-12 16:21:41 +0530

    Create #7maxProduct.java

---
+++ #7maxProduct.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+
+         int max = -1;
+        int smax = -1;
+
+        for(int i=0;i< nums.length;i++)
+        {
+            if(max < nums[i]){
+                smax= max;
+                max=nums[i];
+            }else if(smax < nums[i]){
+                smax = nums[i];
+            }
+        }
+        int ans= (max-1)*(smax-1);
+
+        return ans;
+        
+    }
+}
