bc17659c8e7496be613f7c858479076343fe25c2
Aman Rajak <111343496+amanworld8040@users.noreply.github.com>
2024-09-02 01:36:47 +0530

    Create str.java

---
+++ str.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+
+         if (needle.isEmpty()) {
+            return 0; 
+        }
+        return haystack.indexOf(needle);
+        
+    }
+}
