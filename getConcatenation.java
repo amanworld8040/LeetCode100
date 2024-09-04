47d66d4025f27c2d94e9be8793dfcd5527d6c0c9
Aman Rajak <111343496+amanworld8040@users.noreply.github.com>
2024-09-04 23:58:15 +0530

    Create getConcatenation.java

---
+++ getConcatenation.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int [] ans = new int[2*nums.length];
+
+        for(int i=0;i<nums.length;i++){
+            ans[i] = nums[i];
+        }
+        for(int i=0;i<nums.length;i++){
+            ans[i+nums.length]=nums[i];
+        }
+        
+        return ans;
+        
+    }
+}
